4e66e46e3292374216e48a897715b83a2539421f
shenyang <shenyang@seassoon.com>
2018-05-17 20:30:29 +0800

    本地ip地址

--- src/main/java/com/sy/util/base/IpUtil.java
+++ src/main/java/com/sy/util/base/IpUtil.java
@@ -1,9 +1,13 @@
 package com.sy.util.base;
 
 import java.io.IOException;
+import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 
 /**
  * @deccription 获得ip地址
@@ -15,10 +19,16 @@ public class IpUtil {
 
 	//获得本地ip地址
 	/**
-	 * 此方法不一定是你想要的局域网ip
+	 * 常用的获取ip的方法
 	 */
 	public static String getLocalHost() throws UnknownHostException {
-		return InetAddress.getLocalHost().getHostAddress();
+		 String sysType = System.getProperties().getProperty("os.name");
+		 if(sysType.toLowerCase().startsWith("win")) {  
+			 return InetAddress.getLocalHost().getHostAddress();
+		 }else{
+			 return getIpByEthNum("eth0"); // 兼容Linux
+		 }
+		
 	}
 	
 	/**
@@ -34,7 +44,7 @@ public class IpUtil {
 	
 	/**
 	 * 通过请求域名获得ip
-	 * 这个或许是你想要的
+	 * 若不想获得虚拟机的ip可用这个
 	 */
 	@SuppressWarnings("resource")
 	public static String getLocalHostByHost(String host) throws IOException {
@@ -48,8 +58,37 @@ public class IpUtil {
 		return InetAddress.getByName(host).getHostAddress();
 	}
 	
+	/**
+     * 根据网络接口获取IP地址
+     * @param ethNum 网络接口名，Linux下是eth0
+     * @return
+     */
+    private static String getIpByEthNum(String ethNum) {
+        try {
+            Enumeration allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+            InetAddress ip;
+            while (allNetInterfaces.hasMoreElements()) {
+                NetworkInterface netInterface = (NetworkInterface) allNetInterfaces.nextElement();
+                if (ethNum.equals(netInterface.getName())) {
+                    Enumeration addresses = netInterface.getInetAddresses();
+                    while (addresses.hasMoreElements()) {
+                        ip = (InetAddress) addresses.nextElement();
+                        if (ip != null && ip instanceof Inet4Address) {
+                            return ip.getHostAddress();
+                        }
+                    }
+                }
+            }
+        } catch (SocketException e) {
+        }
+        return "获取服务器IP错误";
+    }
+	
+	
 	public static void main(String[] args) throws IOException {
+		System.out.println(getLocalHost());
 		System.out.println(getHostIp("www.baidu.com"));
 		System.out.println(getLocalHostByHost("www.baidu.com"));
 	}
+	
 }
